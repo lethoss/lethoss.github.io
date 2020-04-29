89a9fa7a18709c157c680501a6a4f7135baf3556
lethoss <59613947+lethoss@users.noreply.github.com>
2020-04-29 12:34:46 +0300

    repair formalization, added css

--- my_first_calc/js/calc_js.js
+++ my_first_calc/js/calc_js.js
@@ -1,35 +1,44 @@
-let string = "";
-let display = document.querySelector('#display');
-let buttons = document.querySelectorAll('button');
-buttons.forEach((item, i) => {
-  item.onclick = function(){
-    if ((display.value == NaN) || (display.value == Infinity)){
-      string=""; display.value=0;
+let string = '';
+let display = document.querySelector ('#display');
+let buttons = document.querySelectorAll ('button');
+buttons.forEach ((item, i) => {
+  item.onclick = function () {
+    if ((display.value == NaN) || (display.value == Infinity)) {
+      string = '';
+      display.value = 0;
     }
-  string = string + item.id;
-  display.value=string;
-  if(item.id == "C"){ string=""; display.value=0;}
-  if(item.id == "B"){
-    string=string.slice(0,string.length-2);
-    display.value=display.value.slice(0,display.value.length-2);}
-if (item.id == "R"){
-  string=string.slice(0,string.length-1);
-  display.value=display.value.slice(0,display.value.length-1);
-for (let i=0; i<string.length; i++){
-  if (string[i]==="+"){
-      display.value = Number(string.slice(0,i)) + Number(string.slice(i+1,string.length));
+    if (item.dataset.role == 'digit'){
+      string = string + item.dataset.digit;
+    } else {
+      string = string + item.dataset.operation;
+    }
+    display.value = string;
+    if (item.dataset.operation == 'c') {
+      string = '';
+      display.value = 0;
+    }
+    if (item.dataset.operation == 'back') {
+      string = string.slice(0, string.length - 5);
+      display.value = display.value.slice (0, display.value.length - 5);
+    }
+    if (item.dataset.operation == 'equally') {
+      string = string.slice (0, string.length - 7);
+      display.value = display.value.slice (0, display.value.length - 7);
+      for (let i = 0; i < string.length; i++) {
+        if (string[i] === '+') {
+          display.value = Number (string.slice(0, i)) + Number (string.slice (i + 1, string.length));
+        }
+        if (string[i] === '-') {
+          display.value = Number (string.slice(0, i)) - Number (string.slice (i + 1, string.length));
+        }
+        if (string[i] === '*') {
+          display.value = Number (string.slice(0, i)) * Number (string.slice (i + 1, string.length));
+        }
+        if (string[i] === '/') {
+          display.value = Number (string.slice(0, i)) / Number (string.slice (i + 1, string.length));
+        }
+        string = display.value;
+      }
+    }
   }
-  if (string[i]==="-"){
-      display.value = Number(string.slice(0,i)) - Number(string.slice(i+1,string.length));
-  }
-  if (string[i]==="*"){
-      display.value = Number(string.slice(0,i)) * Number(string.slice(i+1,string.length));
-  }
-  if (string[i]==="/"){
-      display.value = Number(string.slice(0,i)) / Number(string.slice(i+1,string.length));
-  }
-  string=display.value;
-}
-}
-}
 });
