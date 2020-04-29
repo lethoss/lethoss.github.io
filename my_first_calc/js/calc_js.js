edda8d6a05b10f835f778138c2fb45a5c5714e1d
lethoss <59613947+lethoss@users.noreply.github.com>
2020-04-29 14:47:49 +0300

    working with some actions in 1 display

--- my_first_calc/js/calc_js.js
+++ my_first_calc/js/calc_js.js
@@ -1,4 +1,5 @@
 let string = '';
+let calcArray = [];
 let display = document.querySelector ('#display');
 let buttons = document.querySelectorAll ('button');
 buttons.forEach ((item, i) => {
@@ -24,21 +25,41 @@ buttons.forEach ((item, i) => {
     if (item.dataset.operation == 'equally') {
       string = string.slice (0, string.length - 7);
       display.value = display.value.slice (0, display.value.length - 7);
+      let j = -1;
       for (let i = 0; i < string.length; i++) {
-        if (string[i] === '+') {
-          display.value = Number (string.slice(0, i)) + Number (string.slice (i + 1, string.length));
+        if ((string[i] === '+') || (string[i] === '-') || (string[i] === '*') || (string[i] === '/')) {
+          calcArray.push (Number (string.slice(j + 1, i)), string[i]);
+          j = i;
         }
-        if (string[i] === '-') {
-          display.value = Number (string.slice(0, i)) - Number (string.slice (i + 1, string.length));
-        }
-        if (string[i] === '*') {
-          display.value = Number (string.slice(0, i)) * Number (string.slice (i + 1, string.length));
-        }
-        if (string[i] === '/') {
-          display.value = Number (string.slice(0, i)) / Number (string.slice (i + 1, string.length));
-        }
-        string = display.value;
       }
+      calcArray.push (Number (string.slice (j + 1, i)));
+      for (let i = 0; i < calcArray.length; i++) {
+        if (calcArray[i] == '*') {
+          calcArray.splice(i-1,3,(calcArray[i-1] * calcArray[i+1]));
+          i = 0;
+        }
+      }
+      for (let i = 0; i < calcArray.length; i++) {
+        if (calcArray[i] == '/') {
+          calcArray.splice(i-1,3,(calcArray[i-1] / calcArray[i+1]));
+          i = 0;
+        }
+      }
+      for (let i = 0; i < calcArray.length; i++) {
+        if (calcArray[i] == '+') {
+          calcArray.splice(i-1,3,(calcArray[i-1] + calcArray[i+1]));
+          i = 0;
+        }
+      }
+      for (let i = 0; i < calcArray.length; i++) {
+        if (calcArray[i] == '-') {
+          calcArray.splice(i-1,3,(calcArray[i-1] - calcArray[i+1]));
+          i = 0;
+        }
+      }
+      display.value = calcArray[0];
+      string = calcArray[0];
+      calcArray = [];
     }
   }
 });
